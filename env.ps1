f7d712a86d57f3d22f78b42b2d9dbe579f70b1e3
eric373 <25942990+eric373@users.noreply.github.com>
2022-04-09 22:04:52 -0400

    Use pwsh's Start-Process command

--- env.ps1
+++ env.ps1
@@ -1,4 +1,4 @@
-$Env:PATH="C:\WINDOWS\system32;C:\WINDOWS\system32\wbem;C:\WINDOWS\system32\WindowsPowerShell\v1.0\;D:\kafka_2.13-3.1.0\bin\windows;C:\Program Files\Java\jdk1.8.0_202\bin;D:\Program Files\Git\cmd;D:\Python\Python39\Scripts;D:\Python\Python39;D:\scripts"
+$Env:PATH="C:\WINDOWS\system32;C:\WINDOWS\system32\wbem;D:\kafka_2.13-3.1.0\bin\windows;C:\Program Files\Java\jdk1.8.0_202\bin;D:\Program Files\Git\cmd;D:\Python\Python39\Scripts;D:\Python\Python39;D:\Program Files\Powershell\7\;D:\scripts"
 $Env:JAVA_HOME="C:\Program Files\Java\jdk1.8.0_202"
 $Env:KAFKA_HOME="D:\kafka_2.13-3.1.0"
 
--- start-kafka.ps1
+++ start-kafka.ps1
@@ -5,17 +5,13 @@ $scriptblk = {
     $Host.UI.RawUI.WindowTitle='Kafka Zookeeper';
     zookeeper-server-start.bat "$Env:KAFKA_HOME/config/zookeeper.properties"
 }
-$StartInfo = New-Object System.Diagnostics.ProcessStartInfo
-$StartInfo.FileName = "D:\Program Files\Powershell\7\pwsh.exe"
-$StartInfo.Arguments = '-NoExit' `
-    ,"-Command $scriptblk"
-[System.Diagnostics.Process]::Start($StartInfo)
+Start-Process pwsh -ArgumentList "-NoExit","-Command",$scriptblk
+
+
 
 # Start kafka-server in another powershell session.
 $scriptblk = {
     $Host.UI.RawUI.WindowTitle='Kafka Server';
     kafka-server-start.bat "$Env:KAFKA_HOME/config/server.properties"
 }
-$StartInfo.Arguments = '-NoExit' `
-    ,"-Command $scriptblk"
-[System.Diagnostics.Process]::Start($StartInfo)
+Start-Process pwsh -ArgumentList "-NoExit","-Command",$scriptblk

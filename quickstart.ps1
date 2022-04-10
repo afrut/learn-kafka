5af3532c20608c107d2e09a885108c0e2b7eb790
eric373 <25942990+eric373@users.noreply.github.com>
2022-04-10 14:38:52 -0400

    Use Windows Subsystem for Linux 2 to run kafka cluster. Run clients on windows machine

--- quickstart.ps1
+++ quickstart.ps1
@@ -1,17 +1,25 @@
-# To start the kafka cluster, run these commands in a new window
-zookeeper-server-start.bat "$Env:KAFKA_HOME/config/zookeeper.properties"
-kafka-server-start.bat "$Env:KAFKA_HOME/config/server.properties"
+## To start the kafka cluster, run these commands in a new window
+#zookeeper-server-start.bat "$Env:KAFKA_HOME/config/zookeeper.properties"
+#kafka-server-start.bat "$Env:KAFKA_HOME/config/server.properties"
+
+# Get machine ip of host running WSL2, where kafka is started.
+$hostname = "[::1]"
+$port = 9092
+$socketaddress = "$hostname`:$port"
 
 # Create a topic
-kafka-topics.bat --create --topic quickstart-events --bootstrap-server localhost:9092
+kafka-topics.bat --create --topic quickstart-events --bootstrap-server $socketaddress
 
 # See status of a topic
-kafka-topics.bat --describe --topic quickstart-events --bootstrap-server localhost:9092
+kafka-topics.bat --describe --topic quickstart-events --bootstrap-server $socketaddress
+
+# List available topics int he cluster
+kafka-topics.bat --list --bootstrap-server $socketaddress
 
 # Write some events into the topic using the console producer client
 # This will prompt for some input to write to the topic
-kafka-console-producer.bat --topic quickstart-events --bootstrap-server localhost:9092
+kafka-console-producer.bat --topic quickstart-events --bootstrap-server $socketaddress
 
 # Use the console consumer client to read the topic
 # This will print the contents of the topic
-kafka-console-consumer.bat --topic quickstart-events --from-beginning --bootstrap-server localhost:9092
+kafka-console-consumer.bat --topic quickstart-events --from-beginning --bootstrap-server $socketaddress

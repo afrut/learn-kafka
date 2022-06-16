471ab315fcf03c70f912559829b583e3b41e7e9c
eric373 <25942990+eric373@users.noreply.github.com>
2022-06-16 13:49:45 -0400

    Improve scripts to use arrays

--- env.sh
+++ env.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-export PATH=$PATH+":/mnt/d/kafka_2.13-3.1.0/bin"
-<<<<<<< HEAD
-=======
-export JAVA_HOME="/mnt/c/Program Files/Java/jdk1.8.0_202"
->>>>>>> 303ade1ccc4212c69d5ed457c8fb5dace9dc0e2b
+export PATH=$PATH":/mnt/d/kafka_2.13-3.1.0/bin"
 export KAFKA_HOME="/mnt/d/kafka_2.13-3.1.0"
+IFS=':' read -r -a array <<< $PATH
+for element in "${array[@]}"
+do
+    echo "$element"
+done

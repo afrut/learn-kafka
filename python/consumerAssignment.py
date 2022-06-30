8a61d93b34da41264bdd29b96f61137ccf8bfa4d
eric373 <25942990+eric373@users.noreply.github.com>
2022-06-30 14:27:34 -0400

    Use consumer.seek() and verify offsets after consumption

--- python/consumerAssignment.py
+++ python/consumerAssignment.py
@@ -1,12 +1,11 @@
 #python consumerMisc.py
 import logging
-import threading
 from argparse import ArgumentParser
 from confluent_kafka import Consumer, TopicPartition
 from confluent_kafka.admin import AdminClient
-from time import sleep
 
 from initTopic import initTopic
+from adminInspectOffsets import printOffsets
 
 if __name__ == "__main__":
     server = "[::1]:9092"
@@ -41,10 +40,12 @@ if __name__ == "__main__":
                     if key:
                         key = key.decode("utf-8")
                     value = msg.value().decode("utf-8")
-                    logging.info(f"Received ({key}, {value})")
-            logging.info(f"Processed {N} messages")
+                    # logging.info(f"Received ({key}, {value})")
+            # logging.info(f"Processed {N} messages")
+            return True
         else:
             logging.info("No messages")
+            return False
 
     def assigned(consumer, partitions):
         logging.info(f"assigned() callback executed")
@@ -63,10 +64,24 @@ if __name__ == "__main__":
         while(len(consumer.assignment()) == 0):
             logging.info("Consumer has no assignment yet")
             consumer.poll(timeout)
-        tps = consumer.assignment()
+
+        # Get partitions assigned to this consumer
+        tps = sorted(consumer.assignment(), key = lambda tp: tp.partition)
         logging.info(f"Consumer assigned {len(tps)} partitions:")
+        printOffsets(consumer, tps)
+
+        # Start consuming from half-way point of all partitions
+        wos = {tp.partition: consumer.get_watermark_offsets(tp) for tp in tps}
         for tp in tps:
-            logging.info(f"{' ':4}Partition {tp.partition}")
+            pnum = tp.partition
+            lo = wos[pnum][0]
+            hi = wos[pnum][1]
+            tp.offset = int((lo + hi) / 2)
+            consumer.seek(tp)
+
+        while process(consumer.consume(num_messages = 1, timeout = 1)):
+            pass
+
     except KeyboardInterrupt:
         pass
     # NOTE: Upon call to subscribe(), consumer receives partition assignments
@@ -74,5 +89,6 @@ if __name__ == "__main__":
     # properly executing, ie. (seek(), ). An error is raised when such methods
     # are called before partition assignment completes.
     
+    printOffsets(consumer, tps)
     consumer.close()
     logging.info("Consumer closed.")
